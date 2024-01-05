0a1f422c65fd7b169d352174507f46e75ff2ac91
Ted Bennett <44608108+tedbennett@users.noreply.github.com>
2024-01-05 23:27:00 +0000

    Add packages, axum boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use axum::{routing::get, Router};
+use dotenvy::dotenv;
+
+#[tokio::main]
+async fn main() {
+    let _ = dotenv();
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+    let port = env::var("PORT").unwrap_or("8000".to_string());
+    let address = format!("0.0.0.0:{port}");
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
